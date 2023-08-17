bc8b7e3aa6593b8014b54e75830157c18384db5b
na0260 <samjr.008@gmail.com>
2023-08-17 15:06:47 +0600

    not-found.js added

---
+++ app/not-found.js
@@ -0,0 +1,13 @@
+const NotFound = () => {
+    return (
+        <div className="container mx-auto mt-20">
+            <div className="row">
+                <div className="col-12">
+                    <h1 className="text-center text-3xl font-semibold">Page Not Found</h1>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default NotFound;
