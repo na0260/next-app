efdd51c66dd98eac629fd0f4b9145995a909901b
na0260 <samjr.008@gmail.com>
2023-08-17 15:21:57 +0600

    caching added in comments

--- utils/getPostComments.js
+++ utils/getPostComments.js
@@ -1,5 +1,7 @@
 export default async function getPostComments(id) {
-    const res = await fetch(`https://jsonplaceholder.typicode.com/posts/${id}/comments`);
+    const res = await fetch(`https://jsonplaceholder.typicode.com/posts/${id}/comments`,{
+        cache: "force-cache",
+    });
     if (!res.ok) {
         throw new Error(res.statusText);
     }else {
