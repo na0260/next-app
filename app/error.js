d370b0ab75f730948b3e3f6e609d6acb77db580e
na0260 <samjr.008@gmail.com>
2023-08-17 15:06:24 +0600

    error.js added

---
+++ app/error.js
@@ -0,0 +1,14 @@
+'use client';
+const Error = () => {
+    return (
+        <div className="container mx-auto mt-20">
+            <div className="row">
+                <div className="col-12">
+                    <h1 className="text-center text-3xl font-semibold text-red-600">There is an Error!!!!</h1>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Error;
