9cf64272ef7c76601f18ede028e595388f0a801d
na0260 <samjr.008@gmail.com>
2023-08-17 15:01:06 +0600

    Loader added

---
+++ app/loading.js
@@ -0,0 +1,13 @@
+const Loading = () => {
+    return (
+        <div className="container mx-auto mt-20">
+            <div className="row">
+                <div className="col-12">
+                    <h1 className="text-center text-3xl font-semibold">Loading........</h1>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Loading;
